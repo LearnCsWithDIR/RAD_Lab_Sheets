3c304a4e5b5799eb437183b619412ab9e9b831cb
LearnCsWithDIR <rdinduwara19158@gmail.com>
2023-06-21 22:27:58 +0530

    lab sheet 2 exercise 3 js file

---
+++ lab_sheet_2/ex3.js
@@ -0,0 +1,31 @@
+function exercise_3_1(){
+
+    let area1 = area(7);
+    let perimeter1 = perimeter(7);
+    alert("Area of a circle: "+area1+"\nPerimeter of a circle : "+perimeter1);
+
+}
+
+function area(radius){
+
+    return Math.PI*radius*radius; 
+
+}
+function perimeter(radius){
+
+    return 2*Math.PI*radius;
+
+}
+
+function exercise_3_2(){
+
+    let lenth = 10;
+    alert("Volume of a Cube : "+lenth*lenth*lenth);
+
+}
+
+function exercise_3_3(){
+
+    alert("hello , welcome to our paradise")
+
+}
