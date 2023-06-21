d2dbd0a6dfc20cf9d706186aed82582bb7633f0b
LearnCsWithDIR <rdinduwara19158@gmail.com>
2023-06-21 22:25:24 +0530

    lab sheet 2 exercise 1 js file

---
+++ lab_sheet_2/ex1.js
@@ -0,0 +1,28 @@
+// Exercise_1
+function exercise_1_1(){
+
+    var flowerName;
+    flowerName = "Lili";
+    alert(flowerName);
+
+}
+function exercise_1_2(){
+
+    var x,y,z;
+
+    x = 5;
+    y = 6;
+    z = x+y;
+    alert("x : "+x+"\ny : "+ y+"\nz : "+z);
+
+}
+function exercise_1_3(){
+
+    var price1 = 5;
+    const price2 = 10;
+    let total;
+    total = price1 + price2;
+    alert("price 1 : "+price1+"\nprice 2 : "+ price2+"\ntotal : "+total);
+
+}
+
