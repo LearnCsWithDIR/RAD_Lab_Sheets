30b43dcc604428fb9fa57bc399125648dd73ee4d
Dilum Induwara Rathnayaka <110888028+LearnCsWithDIR@users.noreply.github.com>
2023-08-20 17:21:44 +0530

    added lab sheet 7 files

---
+++ lab_sheet_7/question2.js
@@ -0,0 +1,9 @@
+const numArray = [1,2,3,4,5]
+
+function square(num){
+    return num * num
+}
+
+const sqauredNum = numArray.map(square)
+
+console.log(sqauredNum)
---
+++ lab_sheet_7/question3.js
@@ -0,0 +1,5 @@
+const numbers = [1, 10, 4, 50, 34]
+
+const max = Math.max(...numbers)
+
+console.log("The maximum number is " + max)
